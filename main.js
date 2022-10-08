d96d029ba0eabf3de9af85149e709af96e358d99
Suellem Curvello <109691166+SuellemCurvello@users.noreply.github.com>
2022-10-07 22:27:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+    
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
+//enquanto
+
+// let contador = 0;
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla= listaDeTeclas[contador];
+//     const instrumento= tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`;
+//     console.log(idAudio);
+   
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     };
+//     contador = contador + 1;
+    
+//         console.log(contador);
+//
